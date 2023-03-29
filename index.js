77a6fc349997d8731d54c7e05ecbce562aa6d5ca
JohnnySantosDev <johnnysilvasantos@gmail.com>
2023-03-29 17:17:44 -0400

    new

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
-
+const cors = require('cors');
 app.use(cors());
+app.use(express.json());
 app.get('/conteudos', function (req, res) {
     const acont = [
         {
+            id : "0000001",
             desc: "CRISTIANO RONALDO DESTRÓI, FAZ 2, E PORTUGAL MASSACRA LIECHTENSTEIN NAS ELIMINATÓRIAS PARA A EURO",
             url: "https://www.youtube.com/embed/5wnt8eMGiC8",
             tit: "CR7 da Show"
-        }
-    ]
+        }]
+    
+    console.log(acont)
   res.send(acont)
+
 })
 
-app.listen(process.env.PORT ||3000) 
+app.listen(process.env.PORT ||3001) 
